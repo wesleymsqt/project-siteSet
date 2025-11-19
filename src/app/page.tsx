3980ce774da6c8fb793d469953d1e76cd297555c
Wesley Mesquita <wesleymsqt@alu.ufc.br>
2025-11-19 11:02:46 -0300

    refactor(app-router): Migrate core landing page components to App Router

---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+import { LandingPage } from "@/templates/landing-page";
+
+export default function HomePage() {
+  return <LandingPage />;
+}
--- src/components/active-link/active-link.tsx
+++ src/components/active-link/active-link.tsx
@@ -1,24 +1,27 @@
-import Link, { LinkProps } from "next/link";
-import { useRouter } from "next/router";
-import { ReactNode } from "react";
+"use client";
+
 import { cn } from "@/lib/utils";
+import Link, { LinkProps } from "next/link";
+import { usePathname } from "next/navigation";
 
 type ActiveLinkProps = {
-  children: ReactNode;
+  children: React.ReactNode;
 } & LinkProps;
 
 export const ActiveLink = ({ children, href, ...rest }: ActiveLinkProps) => {
-  const router = useRouter();
-  const isCurrentPath = router.asPath === href || router.asPath === rest.as;
+  const linkPath = (typeof href === "string" ? href : href.pathname) ?? "";
+  const pathname = usePathname();
+  const isActive =
+    pathname === linkPath || pathname?.startsWith(`${linkPath}/`);
 
   return (
     <Link
+      {...rest}
       href={href}
       className={cn(
         "text-action-sm transition-colors hover:text-blue-200",
-        isCurrentPath ? "text-blue-200" : "text-gray-100"
+        isActive ? "text-blue-200" : "text-gray-100"
       )}
-      {...rest}
     >
       {children}
     </Link>
